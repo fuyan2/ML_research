a66182a26e354a60452a3789d5376ce0955bed03
Yan Fu <yan.fu@mail.utoronto.ca>
2019-01-22 16:19:35 -0500

    update inversion loss

--- logistic.py
+++ logistic.py
@@ -21,7 +21,7 @@ learning_rate = 0.1
 loss_beta = 0.003
 BATCH_SIZE = 250
 
-#Universal Setup Construct Dataset
+#Flatten input dataset
 mnist = tf.keras.datasets.mnist
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 x_train = np.reshape(x_train, [x_train.shape[0], -1])
@@ -29,15 +29,17 @@ x_test = np.reshape(x_test, [x_test.shape[0], -1])
 y_train = np.reshape(y_train, [y_train.shape[0], -1])
 y_test = np.reshape(y_test, [y_test.shape[0], -1])
 
+#construct dataset
 features = tf.placeholder(tf.float32, shape=[None, IMG_ROW * IMG_COL])
 labels = tf.placeholder(tf.int32, shape=[None, 1])
 batch_size = tf.placeholder(tf.int64)
-dataset = tf.data.Dataset.from_tensor_slices((features, labels)).batch(batch_size)
+dataset = tf.data.Dataset.from_tensor_slices((features, labels)).batch(batch_size).repeat()
 iter = dataset.make_initializable_iterator()
 x, y_ = iter.get_next()
 y = tf.one_hot(tf.reshape(y_,[-1]), NUM_LABEL)
 
 print(x)
+print(y_)
 print(y)
 print(y_train)
 print(y_train.shape)
@@ -46,9 +48,9 @@ print(x_train.shape)
 xavier_initializer = tf.contrib.layers.xavier_initializer()
 
 def layer(input, num_units):
-  W = tf.Variable(tf.zeros([input.shape[1], num_units]), name="w")
-  B = tf.Variable(tf.zeros([num_units]), name="b")
-  output = tf.matmul(input,W) + B
+  W = tf.Variable(tf.zeros([input.shape[1], num_units], tf.float32), name="w")
+  B = tf.Variable(tf.zeros([num_units], tf.float32), name="b")
+  output = tf.nn.bias_add(tf.matmul(input,W), B)
   return W, B, output
 
 def lrelu(x, alpha):
@@ -77,7 +79,9 @@ def inverter(y, w, b):
 with tf.name_scope("logistic_layer"):
   w,b,z = layer(x,NUM_LABEL)
   y_ml = tf.nn.softmax(z)
-
+  print(w)
+  print(b)
+  print(z)
 #Build Inverter Regularizer
 model_weights = tf.concat([tf.reshape(w,[1, -1]),tf.reshape(b,[1, -1])], 1)
 inv_weights = {
@@ -92,10 +96,10 @@ inv_biases = {
 }
 
 inv_x = inverter(y, w, b)
-
+print(inv_x)
 #Calculate loss
 class_loss = tf.losses.softmax_cross_entropy(y,y_ml)
-inv_loss = tf.losses.mean_squared_error(labels=x, predictions=inv_x)
+inv_loss = tf.losses.mean_squared_error(labels=x, predictions=tf.tanh(inv_x))
 
 # calculate prediction accuracy
 correct = tf.equal(tf.argmax(y_ml, 1), tf.argmax(y, 1))
@@ -104,13 +108,14 @@ accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 def train(loss_beta, learning_rate, Epoch):
   total_loss = class_loss - loss_beta * inv_loss
   model_optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(total_loss, var_list=[w,b])
-  inverter_optimizer = tf.train.GradientDescentOptimizer(0.1).minimize(inv_loss, var_list=[inv_weights, inv_biases])
+  inverter_optimizer = tf.train.GradientDescentOptimizer(0.1).minimize(inv_loss)
   init_vars = tf.global_variables_initializer()
   
   with tf.Session() as sess:
     sess.run(init_vars)
+   
     # initialise iterator with train data
-    sess.run(iter.initializer, feed_dict = {features: x_train, labels: y_train, batch_size: BATCH_SIZE})
+    sess.run(iter.initializer, feed_dict = {features: x_train, labels: y_train, batch_size: 250})
     
     print('Training...')
     for i in range(Epoch):
@@ -122,14 +127,13 @@ def train(loss_beta, learning_rate, Epoch):
     # initialise iterator with test data
     sess.run(iter.initializer, feed_dict = {features: x_test, labels: y_test, batch_size: y_test.shape[0]})
     test_acc = sess.run(accuracy)
-    print('Test accuracy: {:4f}'.format(test_acc))
+    print("beta is %g, test accuracy is %g"%(beta, test_acc))
       
     return test_acc
 
 betas = [0, 0.001, 0.01, 0.1, 0.5, 1., 2., 5., 7., 10., 15., 20.]
 test_accs = np.zeros(len(betas))
 for i,beta in enumerate(betas):
-  test_accs[i] = train(betas,0.01,200)
-  print("beta is %g, test accuracy is %g"%(beta, test_accs[i]))
-  
+  test_accs[i] = train(beta,0.1,200)
+
 plt.plot(betas, test_accs)
