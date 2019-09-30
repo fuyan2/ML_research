dbcfd1c4678f0349bcc364531dcd546fd804af1f
fuyan2 <yanfu1024@gmail.com>
2019-09-30 16:25:48 -0400

    take sys input

--- log_gan_face.py
+++ log_gan_face.py
@@ -333,27 +333,58 @@ def train(beta, model_l2, test, load_model):
             #     return avg_dis, avg_ssim
 
 if __name__ == '__main__':
-    test = 'beta'
+    test = sys.argv[1]
 
-    if test == 'other_people':
-        betas = 0
+    if test == 'avg_img':
+        betas = [0, 5, 10, 20, 30, 40, 60, 70, 80, 90, 100, 120]
         l2_coef = 0.0001
         load_m = False
-        aux_x_data = orl_x_aux
-        aux_y_data = orl_y_train
-        print("aux data size is ", aux_x_data.shape[0])
-        aux_y_data = one_hot(orl_y_train, 20)
-        aux_x_data = aux_x_data[:aux_y_data.shape[0], :]
-        distances, ssims = train(betas, l2_coef, test, load_m)
 
-    elif test == 'avg_img':
-        betas = 0
-        l2_coef = 0.0001
-        load_m = True
+        # use avg image as aux
         aux_x_data = np.repeat(np.reshape(avg_orl_img,[1,x_dim]),orl_y_train.shape[0], axis=0)
         aux_y_data = orl_y_train
         aux_y_data = one_hot(orl_y_train, 20)
         distances, ssims = train(betas, l2_coef, test, load_m)
+
+        gan_distances = np.zeros(len(betas))
+        gan_ssims = np.zeros(len(betas))
+        acc = np.zeros(len(betas))
+        fred_distances = np.zeros(len(betas))
+        fred_ssims = np.zeros(len(betas))
+        i = 0
+        for beta in betas:
+            acc[i], gan_distances[i], gan_ssims[i], fred_distances[i], fred_ssims[i] = train(beta, l2_coef, test+str(beta), load_m)    
+            i += 1
+        np.save('comparison/temp/beta_dis_gan_aiden_aux_avgface', gan_distances)
+        np.save('comparison/temp/beta_acc_aiden_avgface', acc)
+        np.save('comparison/temp/beta_ssim_gan_aiden_aux_avgface', gan_ssims)
+        np.save('comparison/temp/beta_dis_fred_aiden_aux_avgface', fred_distances)
+        np.save('comparison/temp/beta_ssim_fred_aiden_aux_avgface', fred_ssims)
+        # gan_distances = np.load('comparison/temp/beta_dis_gan_aiden_aux_others.npy')
+        # acc = np.load('comparison/temp/beta_acc_aiden_letters.npy')
+        # gan_ssims = np.load('comparison/temp/beta_ssim_gan_aiden_aux_others.npy')
+        # fred_distances = np.load('comparison/temp/beta_dis_fred_aiden_aux_others.npy')
+        # fred_ssims = np.load('comparison/temp/beta_ssim_fred_aiden_aux_others.npy')
+        plt.close()
+        plt.plot(betas, gan_distances, label='gan_distances')
+        plt.plot(betas, fred_distances, label='fred_distances')
+        plt.xlabel('model beta coefficient')
+        plt.ylabel('sq distance between mi and avg')
+        plt.legend(loc='best')
+        plt.savefig('comparison/beta_vs_sq_dis_aiden_avgimg_aux_avgface.png')
+        plt.close()
+        plt.plot(betas, acc)
+        plt.xlabel('model beta coefficient')
+        plt.ylabel('accuracy')
+        plt.legend(loc='best')
+        plt.savefig('comparison/beta_vs_accuracy_aiden_avgimg_aux_avgface.png')
+        plt.close()
+        plt.plot(betas, gan_ssims, label='gan_ssims')
+        plt.plot(betas, fred_ssims, label='fred_ssims')
+        plt.xlabel('model beta coefficient')
+        plt.ylabel('ssim between mi and avg')
+        plt.legend(loc='best')
+        plt.savefig('comparison/beta_vs_ssims_aiden_avgimg_aux_avgface.png')
 
     elif test == 'no_aux':
         betas = 0
@@ -365,13 +396,11 @@ if __name__ == '__main__':
         aux_y_data = one_hot(orl_y_train, 10)
         distances, ssims = train(betas, l2_coef, test, load_m)
 
-    elif test == 'beta':
+    elif test == 'others':
         betas = [0, 5, 10, 20, 30, 40, 60, 70, 80, 90, 100, 120]
-        # betas = [0, 0.01, 0.1, 0.5, 1., 2., 5., 7., 10., 15., 20.]
         l2_coef = 0.0001
-        
-        # Use letters as aux
-        load_m = False
+
+        # Use other people as aux    
         aux_x_data = orl_x_aux
         aux_y_data = orl_y_train
         print("aux data size is ", aux_x_data.shape[0])
@@ -388,7 +417,7 @@ if __name__ == '__main__':
             acc[i], gan_distances[i], gan_ssims[i], fred_distances[i], fred_ssims[i] = train(beta, l2_coef, test+str(beta), load_m)    
             i += 1
         np.save('comparison/temp/beta_dis_gan_aiden_aux_others', gan_distances)
-        np.save('comparison/temp/beta_acc_aiden_letters', acc)
+        np.save('comparison/temp/beta_acc_aiden_aux_others', acc)
         np.save('comparison/temp/beta_ssim_gan_aiden_aux_others', gan_ssims)
         np.save('comparison/temp/beta_dis_fred_aiden_aux_others', fred_distances)
         np.save('comparison/temp/beta_ssim_fred_aiden_aux_others', fred_ssims)
